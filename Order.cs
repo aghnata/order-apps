416bf857a75c606e7a0cd0bdd724856abcf54675
aghnat.atqiya <aghnat.atqiya@xtremax.com>
2023-08-23 02:44:18 +0700

    upload project

---
+++ Order.cs
@@ -0,0 +1,91 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+public interface IOrder
+{
+	string Name { get; set; }
+	int Price { get; set; }
+}
+
+public class Order : IOrder
+{
+	public string Name { get; set; }
+	public int Price { get; set; }
+}
+
+public interface IOrderSystem
+{
+	void AddToCart(IOrder order);
+	void RemoveFromCart(IOrder order);
+	int CalculateTotalAmount();
+	List<(int, string)> CategoryDiscounts();
+	List<(string, int)> CartItems();
+}
+
+public class OrderSystem : IOrderSystem
+{
+	public List<(string, int)> Carts {get; set;} = new List<(string, int)> ();
+	public List<(int, string)> DiscountCategories {get; set;} = new List<(int, string)> ();
+	
+	public void AddToCart(IOrder order) 
+	{
+		Carts!.Add(new (order.Name, order.Price) );	
+	}
+
+	public void RemoveFromCart(IOrder order) 
+	{
+		Carts!.Remove(new(order.Name, order.Price));
+	}
+
+	public int CalculateTotalAmount() 
+	{
+		int total = 0;
+
+		foreach (var sub in CartItems()) 
+		{
+			int hargaDiskon = sub.Item2;
+			if (sub.Item2 <= 10)
+			{
+				hargaDiskon = sub.Item2 - (sub.Item2 * 10 / 100);
+				DiscountCategories.Add(new (sub.Item2, "Cheap"));
+			}
+            else if (sub.Item2 > 10 && sub.Item2 <= 20) 
+            {
+				hargaDiskon = sub.Item2 - (sub.Item2 * 20 / 100);
+				DiscountCategories.Add(new(sub.Item2, "Medium"));
+			}
+			else if (sub.Item2 > 20)
+			{
+				hargaDiskon = sub.Item2 - (sub.Item2 * 30 / 100);
+				DiscountCategories.Add(new(sub.Item2, "Expensive"));
+			}
+
+			total += hargaDiskon;
+		}
+
+		return total;
+	}
+
+	public List<(int, string)> CategoryDiscounts() 
+	{
+		return DiscountCategories;
+	}
+
+	public List<(string, int)> CartItems() 
+	{
+		return Carts;
+	}
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Test
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			IOrderSystem orderSystem = new OrderSystem();
+
+			Console.WriteLine("Total Input:");
+			int oCount = Convert.ToInt32(Console.ReadLine().Trim());
+			for (int i = 1; i <= oCount; i++)
+			{
+				Console.WriteLine($"Input item {i}:");
+				var a = Console.ReadLine().Trim().Split(" ");
+				IOrder e = new Order();
+				e.Name = a[0];
+				e.Price = Convert.ToInt32(a[1]);
+				orderSystem.AddToCart(e);
+			}
+
+			Console.WriteLine("Total Remove:");
+			int oRemove = Convert.ToInt32(Console.ReadLine().Trim());
+			for (int i = 1; i <= oRemove; i++)
+			{
+				Console.WriteLine($"Remove item {i}:");
+				var a = Console.ReadLine().Trim().Split(" ");
+				IOrder e = new Order();
+				e.Name = a[0];
+				e.Price = Convert.ToInt32(a[1]);
+				orderSystem.RemoveFromCart(e);
+			}
+
+			int totalAmount = orderSystem.CalculateTotalAmount();
+			Console.WriteLine("Total Amount: " + totalAmount);
+
+			var categoryDiscounts = orderSystem.CategoryDiscounts();
+			foreach (var categoryDiscount in categoryDiscounts.OrderBy(a => a.Item1))
+			{
+				Console.WriteLine(categoryDiscount.Item1 + " Category Discount: " + categoryDiscount.Item2);
+			}
+
+			var cartItems = orderSystem.CartItems();
+			foreach (var food in cartItems.OrderBy(a => a.Item1))
+			{
+				Console.WriteLine(food.Item1 + " (" + food.Item2 + " items)");
+			}
+
+		}
+	}
+}
